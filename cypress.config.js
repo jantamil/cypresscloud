393c9eed16b7b0d042ffe09d5aab0aee4b452497
Jane <jananeemani99@gmail.com>
2025-11-27 12:11:54 +0530

    new

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId:"xfe9eo",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    projectId:"6842m1"
+    
   },
 });
